ed9152f056021af4b0f138a78de6559a8c7850a5
Grant Lewis <glewis@threewill.com>
2021-03-04 13:57:41 -0500

    Change font color to match logo

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
+      <div className="min-h-screen text-hh-gray">
         <Alert />
         <main>{children}</main>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         'accent-7': '#333',
         success: '#0070f3',
         cyan: '#79FFE1',
+        'hh-gray': '#7F7F7F'
       },
       spacing: {
         28: '7rem',
